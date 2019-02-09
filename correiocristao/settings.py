3b1484a168c0522b1617c33007bfac76a0b44305
José Medeiros <00archangel00@gmail.com>
2019-02-09 12:50:41 -0200

    Correção de bugs

--- correiocristao/settings.py
+++ correiocristao/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['correio-cristao.herokuapp.com', 'localhost', '127.0.0.1', 'www.correiocristao.com.br', 'correiocristao.com.br', ]
+ALLOWED_HOSTS = ['correio-cristao.herokuapp.com', 'localhost', '127.0.0.1', ]
 
 # Application definition
 
