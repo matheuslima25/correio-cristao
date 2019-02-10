80f5ed77c5a6aae90c33f247831a6cd84fed1014
José Medeiros <00archangel00@gmail.com>
2019-02-10 14:40:15 -0200

    Update settings

--- correiocristao/settings.py
+++ correiocristao/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = (config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['correio-cristao.herokuapp.com', 'localhost', '127.0.0.1', 'correiocristao.com.br', 'www.correiocristao.com.br', ]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
